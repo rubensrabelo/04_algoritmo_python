bfaec28eb18831adec8e91c1438f6fd558b54e30
Rubens Rabelo <rubensrabelo123@gmail.com>
2023-05-05 19:29:52 -0300

    qlqr commit

--- p002_arquivo.py
+++ p002_arquivo.py
@@ -1,9 +1,29 @@
-arq = open(r"arq.txt", "r")
+#Abrir arquivo TXT
 
-#print(type(arq))
+#arq = open('arq.txt', 'r') # r = read
 
-print(arq.read())
+#print(arq.read()) 
 
-print(arq.seek(0,0))
+#print(arq.tell())
 
-print(arq.tell())
+#print(arq.seek(0,0))
+
+#arq = open('arq2.txt', 'w') # w = write
+
+#arq.write('Escrevendo quaisquer coisa, e continuando escrevendo.')
+
+#arq.close()
+
+#arq2 = open('arq2.txt', 'r')
+
+#print(arq2.read())
+
+arq3 = open('arq2.txt', 'a')
+
+arq3.write('outra frase')
+
+arq3.close()
+
+arq4 = open('arq2.txt', 'r')
+
+print(arq4.read())
